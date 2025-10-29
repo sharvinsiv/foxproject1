a8aa192cc268b06f909ca6b5b8d7845280899dfb
S <svs7676@psu.edu>
2025-10-29 11:50:01 -0400

    Update Project-1.js

--- src/Project-1.js
+++ src/Project-1.js
@@ -1,14 +1,12 @@
-import { LitElement, html } from 'lit';
-import './src/fox-photo.js';
+import fetch from 'node-fetch';
 
-export class Project1 extends LitElement {
-  render() {
-    return html`
-      <fox-photo></fox-photo>
-    `;
-  }
+export default async function handler(request, res) {
+  const url = `https://randomfox.ca/floof/`;
+  const currentFox = await fetch(url).then(res => res.json());
+  res.setHeader('Cache-Control', 'max-age=0, s-maxage=1800');
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers", "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+  res.json(await currentFox);
 }
-
-customElements.define('project-1', Project1);
-
-
